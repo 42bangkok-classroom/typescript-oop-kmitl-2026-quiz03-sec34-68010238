fbb1cbf07fd21b8d1ef99b64f4c2d01d524ef32f
bank <68010238@kmitl.ac.th>
2026-02-20 17:49:56 +0700

    petch

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,17 @@
-export class Car {}
+export class Car {
+  public brand!: string;
+  public model!: string;
+
+  public start(): void {
+    console.log(this.brand + ' ' + this.model + ' is starting...');
+  }
+  public accelerate(): void {
+    console.log(this.brand + ' ' + this.model + ' is accelerating...');
+  }
+  public brake(): void {
+    console.log(this.brand + ' ' + this.model + ' is braking...');
+  }
+  public stop(): void {
+    console.log(this.brand + ' ' + this.model + ' is stopping...');
+  }
+}
