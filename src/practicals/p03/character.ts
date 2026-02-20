478d57f985985d37540a1098a1bca2c9eab9c4bb
bank <68010238@kmitl.ac.th>
2026-02-20 18:36:45 +0700

    pp

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,24 @@
-export class Character {}
+export class Character {
+    protected name: string;
+  protected health: number = 100;
+
+  constructor(name: string, health?: number){
+    this.name = name;
+    if (health !== undefined){
+      this.health = health;
+    }
+  }
+  public getName(): string{
+    return this.name;
+  }
+  public getHealth(): number{
+    return this.health;
+  }
+  public receiveDamage(damage: number): void{
+    this.health -= damage;
+
+    if (this.health < 0){
+      this.health = 0;
+    }
+  }
+}
--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -2,4 +2,16 @@ import { Character } from "./character";
 
 export class Warrior extends Character {
   // TODO: implement class properties, constructor with super(...), and methods
+  private weapon: string;
+  constructor(name: string, weapon: string, health: number) {
+    super(name, health);
+    this.weapon = weapon;
+  }
+  public getWeapon(): string {
+    return this.weapon;
+  }
+  public receiveDamage(damage: number): void {
+    const reducedDamage = damage - (damage * 0.9);
+    super.receiveDamage(reducedDamage);
+  }
 }
